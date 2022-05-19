6b803f16bab3dd9c92fb20ae6f4747a9333392e4
Гуляев Антон <63814006+GuliaevAE@users.noreply.github.com>
2022-05-19 19:17:46 +0300

    +

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 let order = document.querySelector('#order')
 let modalOrder = document.querySelector('#modalOrder')
 let modalWindow = document.querySelector('#window')
+let modalMenu = document.querySelector('#modalMenu')
+let closeForModalMenu = document.querySelector('#closeForModalMenu')
+let closeForModalOrder = document.querySelector('#closeForModalOrder')
+let menuButton = document.querySelector('#menuButton')
 let back = document.querySelector('.back')
-order.addEventListener("click", ClickOrder)
-modalOrder.addEventListener("click", (e)=>{
-    
-    if(e.target!==modalWindow && modalOrder.classList == "shadow active"){
-        modalOrder.classList ="shadow"
+
+order.addEventListener("click", ()=>modalOrder.classList = "shadow active")
+menuButton.addEventListener("click", ()=>modalMenu.classList = "shadow active")
+
+modalOrder.addEventListener("click", (e) => {
+
+    if (e.target === modalOrder || e.target === closeForModalOrder) {
+        modalOrder.classList = "shadow"
+        
+        
     }
 })
 
-function ClickOrder(){
-    modalOrder.classList ="shadow active"
-}
+modalMenu.addEventListener("click", (e)=>{
+    if (e.target === modalMenu || e.target === closeForModalMenu) {
+        modalMenu.classList = "shadow"
+    }
+})
+
